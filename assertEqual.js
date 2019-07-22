692eaa5ad74da87be07448ff9b05c58db8ef7c6e
Chen Liang <c.liang@ualberta.ca>
2019-07-22 18:59:22 +0000

    last edit

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    return console.log(`✅✅✅ Assertion Failed: ${actual} === ${expected}`);
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
   }
 };
 
