1e1ab5a2ed4edc1e496e72c5e07b5860be47cd57
Chen Liang <c.liang@ualberta.ca>
2019-07-23 22:00:57 +0000

    deleting useless words

---
+++ without.js
@@ -0,0 +1,57 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+
+const assertArraysEqual = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+    }
+  }
+  return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);;
+};
+
+// TEST CODE
+// assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+// assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+
+const without = function(array, values) {
+  let answer = array;
+for(let i = 0; i < array.length; i++){
+  for(let j = 0; j < values.length; j++){
+if(array[i] === values[j])
+answer.splice(i,1)
+}
+}
+return answer;
+}
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
