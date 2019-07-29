9216f27f0d3f3f4cdb9a5288ee82077ab481287f
Chen Liang <c.liang@ualberta.ca>
2019-07-29 18:54:22 +0000

    refactored for module export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,10 @@
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-    }
+  if (eqArrays(actual, expected)) {
+  return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
-  return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);;
+  return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,56 +1,15 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
-const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-
-const assertArraysEqual = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-    }
-  }
-  return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);;
-};
-
-// TEST CODE
-// assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-// assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
-
 const middle = function(array) {
-  let mid = Math.floor(array.length/2);
-  if(array.length <= 2 )
-    return console.log([])
-  else if(array.length % 2 !== 0)
-  return console.log(array[mid])  
-  else if(array.length % 2 ===0)
-  return console.log(array[mid], array[mid + 1])
+  let answer = [];
+  let mid = Math.floor(array.length / 2);
+  if (array.length > 2) {
+    if (array.length % 2 !== 0) {
+      answer.push(array[mid]);
+    } else {
+      answer.push(array[(mid) - 1]);
+      answer.push(array[(mid)]);
+    }
 }
+return answer;
+};
 
-
-middle([1]) // => []
-middle([1, 2]) // => [] 
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+module.exports = middle;
