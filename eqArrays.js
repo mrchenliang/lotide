8d28b862dd89452c22b60895b5f8a3e0b93cd3e0
Chen Liang <c.liang@ualberta.ca>
2019-08-22 07:21:49 +0000

    updating eqArray to include nested arrays that are identical

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,30 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(actual, expected) {
-  
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
+const eqArrays = function(array1, array2) {
+  if (
+    !(array1 instanceof Array) ||
+    !(array2 instanceof Array) ||
+    array1.length !== array2.length
+  ) {
+    return false;
+  } else {
+    let index = array1.length;
+    while (index > 0) {
+      index--;
+      if (array1[index] instanceof Array || array2[index] instanceof Array) {
+        if (!eqArrays(array1[index], array2[index])) {
+          return false;
+        }
+      } else if (array1[index] !== array2[index]) {
+        return false;
+      }
     }
+    return true;
   }
-
-  return true;
 };
 
 module.exports = eqArrays;
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
